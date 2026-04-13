5192074db0480413bce0597dbee06e00f17791f6
Shahzain333 <shahzainkhannaizi1234@gmail.com>
2026-04-13 11:27:45 +0500

    Setup Routes Between Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,36 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import MenuDetails from './pages/MenuDetails'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import BookTable from './pages/BookTable'
+import MyBooking from './pages/MyBooking'
+import MyOrder from './pages/MyOrder'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Contact from './pages/Contact'
 
 function App() {
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/menu' element={<Menu/>}/>
+        <Route path='/menu-details/:id' element={<MenuDetails/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
+        <Route path='/book-table' element={<BookTable/>}/>
+        <Route path='/my-bookings' element={<MyBooking/>}/>
+        <Route path='/my-orders' element={<MyOrder/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+    </div>
   )
 }
 
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import useNavigate from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export const AppContext = createContext()
 
