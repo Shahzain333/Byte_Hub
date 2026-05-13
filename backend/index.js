6efaaaaf0082636f880ec6a1191508b7b77642e6
Shahzain333 <shahzainkhannaizi1234@gmail.com>
2026-05-13 14:02:10 +0500

    Changes in Backend for Deployment URL Frontend Vercel

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ connectCloudinary()
 // Middleware
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://byte-hub-resturant-frontend.vercel.app",
     credentials: true
 }))
 app.use(cookieParser())
